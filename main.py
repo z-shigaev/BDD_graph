48e29d85a3015aebed5ea4635208858addff18de
Zamir Shigaev <z.shigaev@g.nsu.ru>
2021-09-08 17:51:48 +0700

    Создание Config-файла. Настройка сохранения и получения параметров из данного файла.

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import main_win
 import plot_canvas as pc
 import sys
 import numpy as np
-import math as mth
+import configparser
+#import matplotlib.backend_bases.FigureCanvasBase as Fig
+#import math as mth
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
 
 from PyQt5 import QtWidgets, QtCore, QtGui
 
@@ -26,46 +32,69 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
         self.y = []
         self.widget1_layout = pc.Layout(self.widget1, self.x, self.y)
         self.widget1.setLayout(self.widget1_layout)
+        self.widget1_layout.draw_graph(self.x, self.y)
         self.widget2_layout = pc.Layout(self.widget2, self.x, self.y)
         self.widget2.setLayout(self.widget2_layout)
+        self.widget2_layout.draw_graph(self.x, self.y)
         self.widget3_layout = pc.Layout(self.widget3, self.x, self.y)
         self.widget3.setLayout(self.widget3_layout)
-#        self.widget2_layout = pc.Layout(self.widget2)
-#        self.widget2.setLayout(self.widget2_layout)
-#        self.widget3_layout = pc.Layout(self.widget3)
-#        self.widget3.setLayout(self.widget3_layout)
-
+        self.widget3_layout.draw_graph(self.x, self.y)
+        # self.widget2_layout = pc.Layout(self.widget2)
+        # self.widget2.setLayout(self.widget2_layout)
+        # self.widget3_layout = pc.Layout(self.widget3)
+        # self.widget3.setLayout(self.widget3_layout)
+        #
         # Инициализация переменных графиков
-        self.g1_par1 = 0
-        self.g1_par2 = 0
-        self.g1_par3 = 0
-        self.g1_min = 0
-        self.g1_max = 0
-        self.g1_coeff = 0
-        self.g1_step = 0
+        self.g1_par1 = float(config["Graph_1"]["par1"])
+        self.dSB_par11.setProperty("value", self.g1_par1)
+        self.g1_par2 = float(config["Graph_1"]["par2"])
+        self.dSB_par12.setProperty("value", self.g1_par2)
+        self.g1_par3 = float(config["Graph_1"]["par3"])
+        self.dSB_par13.setProperty("value", self.g1_par3)
+        self.g1_min = float(config["Graph_1"]["min"])
+        self.dSB_min1.setProperty("value", self.g1_min)
+        self.g1_max = float(config["Graph_1"]["max"])
+        self.dSB_max1.setProperty("value", self.g1_max)
+        self.g1_coeff = float(config["Graph_1"]["coeff"])
+        self.dSB_coeff1.setProperty("value", self.g1_coeff)
+        self.g1_step = float(config["Graph_1"]["step"])
+        self.dSB_step1.setProperty("value", self.g1_step)
         self.x1 = []
         self.y1 = []
         #
-        self.g2_par1 = 0
-        self.g2_par2 = 0
-        self.g2_par3 = 0
-        self.g2_min = 0
-        self.g2_max = 0
-        self.g2_coeff = 0
-        self.g2_step = 0
+        self.g2_par1 = float(config["Graph_2"]["par1"])
+        self.dSB_par21.setProperty("value", self.g2_par1)
+        self.g2_par2 = float(config["Graph_2"]["par2"])
+        self.dSB_par22.setProperty("value", self.g2_par2)
+        self.g2_par3 = float(config["Graph_2"]["par3"])
+        self.dSB_par23.setProperty("value", self.g2_par3)
+        self.g2_min = float(config["Graph_2"]["min"])
+        self.dSB_min2.setProperty("value", self.g2_min)
+        self.g2_max = float(config["Graph_2"]["max"])
+        self.dSB_max2.setProperty("value", self.g2_max)
+        self.g2_coeff = float(config["Graph_2"]["coeff"])
+        self.dSB_coeff2.setProperty("value", self.g2_coeff)
+        self.g2_step = float(config["Graph_2"]["step"])
+        self.dSB_step2.setProperty("value", self.g2_step)
         self.x2 = []
         self.y2 = []
         #
-        self.g3_par1 = 0
-        self.g3_par2 = 0
-        self.g3_par3 = 0
-        self.g3_min = 0
-        self.g3_max = 0
-        self.g3_coeff = 0
-        self.g3_step = 0
+        self.g3_par1 = float(config["Graph_3"]["par1"])
+        self.dSB_par31.setProperty("value", self.g3_par1)
+        self.g3_par2 = float(config["Graph_3"]["par2"])
+        self.dSB_par32.setProperty("value", self.g3_par2)
+        self.g3_par3 = float(config["Graph_3"]["par3"])
+        self.dSB_par33.setProperty("value", self.g3_par3)
+        self.g3_min = float(config["Graph_3"]["min"])
+        self.dSB_min3.setProperty("value", self.g3_min)
+        self.g3_max = float(config["Graph_3"]["max"])
+        self.dSB_max3.setProperty("value", self.g3_max)
+        self.g3_coeff = float(config["Graph_3"]["coeff"])
+        self.dSB_coeff3.setProperty("value", self.g3_coeff)
+        self.g3_step = float(config["Graph_3"]["step"])
+        self.dSB_step3.setProperty("value", self.g3_step)
         self.x3 = []
         self.y3 = []
-        #
         # Обработка нажатий на кнопки
         self.btn_val1.clicked.connect(self.find_value1)
         self.btn_val2.clicked.connect(self.find_value2)
@@ -74,8 +103,8 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
         self.btn_create1.clicked.connect(self.create_plot1)
         self.btn_create2.clicked.connect(self.create_plot2)
         self.btn_create3.clicked.connect(self.create_plot3)
-
-
+        #
+        #self.btn_press_event_id = self.widget1_layout.canvas.mpl_connect('btn_press_event', Fig.onMouseEvent)
 
     def resizeEvent(self, event):
         print("Window has been resized")
@@ -158,9 +187,8 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
                 self.x1 = self.x
                 self.y1 = self.y
                 print(4)
+                self.update_config(1)
                 pass
-
-
 
     def create_plot2(self):
         self.g2_par1 = self.dSB_par21.value()
@@ -184,10 +212,9 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
                 self.widget2_layout.draw_graph(self.x, self.y)
                 self.x2 = self.x
                 self.y2 = self.y
+                self.update_config(2)
                 print(5)
                 pass
-
-
 
     def create_plot3(self):
         self.g3_par1 = self.dSB_par31.value()
@@ -211,8 +238,44 @@ class MainWindow(QtWidgets.QMainWindow, main_win.Ui_MainWindow):
                 self.widget3_layout.draw_graph(self.x, self.y)
                 self.x3 = self.x
                 self.y3 = self.y
+                self.update_config(3)
                 print(6)
                 pass
+
+    def update_config(self, mode):
+        path = ""
+        if mode == 1:
+            path = "Graph_1"
+            config.set(path, "par1", str(self.g1_par1))
+            config.set(path, "par2", str(self.g1_par2))
+            config.set(path, "par3", str(self.g1_par3))
+            config.set(path, "min", str(self.g1_min))
+            config.set(path, "max", str(self.g1_max))
+            config.set(path, "coeff", str(self.g1_coeff))
+            config.set(path, "step", str(self.g1_step))
+        elif mode == 2:
+            path = "Graph_2"
+            config.set(path, "par1", str(self.g2_par1))
+            config.set(path, "par2", str(self.g2_par2))
+            config.set(path, "par3", str(self.g2_par3))
+            config.set(path, "min", str(self.g2_min))
+            config.set(path, "max", str(self.g2_max))
+            config.set(path, "coeff", str(self.g2_coeff))
+            config.set(path, "step", str(self.g2_step))
+        else:
+            path = "Graph_3"
+            config.set(path, "par1", str(self.g3_par1))
+            config.set(path, "par2", str(self.g3_par2))
+            config.set(path, "par3", str(self.g3_par3))
+            config.set(path, "min", str(self.g3_min))
+            config.set(path, "max", str(self.g3_max))
+            config.set(path, "coeff", str(self.g3_coeff))
+            config.set(path, "step", str(self.g3_step))
+        with open("config.ini", 'w') as configfile:
+            config.write(configfile)
+
+    def btn_press_event(self):
+        print("899")
 
 
 
--- plot_canvas.py
+++ plot_canvas.py
@@ -6,8 +6,9 @@ from PyQt5.QtWidgets import QApplication, QVBoxLayout, QMainWindow, QTableWidget
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+#from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 
 
@@ -29,6 +30,11 @@ class Layout(QVBoxLayout):
 
     def draw_graph(self, x1, y1):
         try:
+            xlabel = 8
+            ylabel = 8
+            xlabelpad = 0
+            ylabelpad = xlabelpad
+            #
             self.figure.clear()
             #
             self.ax1 = self.figure.add_subplot(211)
@@ -36,25 +42,34 @@ class Layout(QVBoxLayout):
             self.ax3 = self.figure.add_subplot(224)
             #
             self.ax1.plot(x1, y1)
-            self.ax1.set_xlabel('Ось X', fontsize=10, labelpad=0)
-            self.ax1.set_ylabel('Ось Y', fontsize=10, labelpad=0)
+            #
+            #self.ax1.xaxis.set_major_locator(ticker.MultipleLocator(2))
+            #self.ax1.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+            #self.ax1.yaxis.set_major_locator(ticker.MultipleLocator(50))
+            #self.ax1.yaxis.set_minor_locator(ticker.MultipleLocator(10))
+            #
+            self.ax1.tick_params(labelsize=6)
+            self.ax1.set_xlabel('Ось X', fontsize=xlabel, labelpad=xlabelpad)
+            self.ax1.set_ylabel('Ось Y', fontsize=ylabel, labelpad=ylabelpad)
             self.ax1.spines['right'].set_visible(False)
             self.ax1.grid(color='black', linewidth=1, linestyle='--')
             #
             #self.ax1.plot(x1, y1)
-            self.ax2.set_xlabel('Ось X', fontsize=10, labelpad=0)
-            self.ax2.set_ylabel('Ось Y', fontsize=10, labelpad=0)
+            self.ax2.tick_params(labelsize=6)
+            self.ax2.set_xlabel('Ось X', fontsize=xlabel, labelpad=xlabelpad)
+            self.ax2.set_ylabel('Ось Y', fontsize=ylabel, labelpad=ylabelpad)
             self.ax2.spines['right'].set_visible(False)
             self.ax2.grid(color='black', linewidth=1, linestyle='--')
             #
             #self.ax1.plot(x1, y1)
-            self.ax3.set_xlabel('Ось X', fontsize=10, labelpad=0)
-            self.ax3.set_ylabel('Ось Y', fontsize=10, labelpad=0)
+            self.ax3.tick_params(labelsize=6)
+            self.ax3.set_xlabel('Ось X', fontsize=xlabel, labelpad=xlabelpad)
+            self.ax3.set_ylabel('Ось Y', fontsize=ylabel, labelpad=ylabelpad)
             self.ax3.spines['right'].set_visible(False)
             self.ax3.grid(color='black', linewidth=1, linestyle='--')
             self.canvas.draw()
         except Exception as error:
-            print("Исключение " + str(error))
+            print("Поймано исключение " + str(error))
 
 
 
